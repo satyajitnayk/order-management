c8828ab3ed4107ae578db5dacc8c63f1a2401410
satyajit nayak <satyajitn15@gmail.com>
2023-12-23 15:48:40 +0530

    add successful orderes to kafka queue

--- config/payment.js
+++ config/payment.js
@@ -1,7 +1,11 @@
+const {delay} = require("../utility");
+
 /**
  * it returns success 70% of time
  * @returns {boolean}
  */
-export function checkPayment({txnId, orderId}) {
+async function checkPayment({ txnId, orderId }) {
+  await delay(2000)
   return Math.random() < 0.7;
 }
+module.exports = {checkPayment}
--- index.js
+++ index.js
@@ -1,23 +1,48 @@
-const Fastify = require('fastify');
 const { setupDatabase } = require('./config/db.js');
 const { userRoutes } = require('./routes/users.js');
+const { kafka } = require('./config');
+const {checkPayment} = require("./config/payment");
 
-const fastify = Fastify({
+const fastify = require('fastify')({
   logger: true,
 });
 
-setupDatabase(fastify);
-userRoutes(fastify);
+async function runServer() {
+  try {
+    setupDatabase(fastify);
+    userRoutes(fastify);
 
-fastify.get('/health', async (request, reply) => {
-  reply.type('application/json').code(200);
-  return { status: 'ok', uptime: process.uptime() };
-});
+    // Initialize Kafka
+    await kafka.initializeKafka();
 
-fastify.listen({ port: process.env.PORT }, (err, address) => {
-  if (err) {
-    fastify.log.error(err);
+    fastify.get('/health', async (request, reply) => {
+      reply.type('application/json').code(200);
+      return { status: 'ok', uptime: process.uptime() };
+    });
+
+    fastify.post('/orders', async (request, reply) => {
+      try {
+        const order = request.body;
+        const {orderId= "1234", txnId= "BEY23DD2"} = order;
+        const isPaymentSuccess = await checkPayment({orderId,txnId})
+        if(isPaymentSuccess) {
+          await kafka.produceOrder(order);
+          reply.send({ success:true, message: `order with orderId ${orderId} placed successfully` });
+        } else {
+          reply.status(400).send({success:false, message: "payment failed"});
+        }
+      } catch (error) {
+        reply.status(500).send({ message: 'Error producing order' });
+      }
+    });
+
+    // Use await or return a promise for the listen operation
+    await fastify.listen({ port: process.env.PORT });
+    console.log(`Server listening on ${fastify.server.address().port}`);
+  } catch (error) {
+    console.error('Error starting the server:', error);
     process.exit(1);
   }
-  fastify.log.info(`Server listening on ${address}`);
-});
+}
+
+runServer();
